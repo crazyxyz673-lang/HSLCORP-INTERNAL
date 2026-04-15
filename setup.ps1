309cdaba22f99a94b91acc17cc096336b34bb2d4
crazyxyz673-lang <crazyxyz673@gmail.com>
2026-04-15 09:35:38 +0530

    Delete setup.ps1

--- setup.ps1
+++
@@ -1,9 +0,0 @@
-Write-Host "Setup Started..."
-
-$url = "https://raw.githubusercontent.com/crazyxyz673-lang/HSLCORP-INTERNAL/main/JATIN.exe"
-$output = "$env:TEMP\HSL-CORP.exe"
-
-Invoke-WebRequest -Uri $url -OutFile $output
-
-Write-Host "Running App..."
-Start-Process $output
